fa115b2f3b126ec4e08d5cc95de0f601f9132f77
Juan Pablo Ascencio <jpabloascencioglez@gmail.com>
2023-11-03 21:33:43 -0600

    knasdkcansdc, hated task 3 with my soul ngl

--- solutions/task3.js
+++ solutions/task3.js
@@ -1,4 +1,26 @@
-function resolve({ A, B, C, D, E, F }) { }
+function resolve({ A, B, C, D, E, F }) {
+    /**
+     A   B
+    / \ /|
+   C   D |
+    \ / \|
+    E   F
+    */
+
+    // this one almost makes me cry ❤️‍🩹
+
+    Promise.all([A(), B()]).then(([a, b]) => {
+        Promise.all([
+            C(a),
+            D(a, b).then((d) => {
+                F(b, d);
+                return d;
+            }),
+        ]).then(([c, d]) => {
+            E(c, d);
+        });
+    });
+}
 
 if (require.main === module) {
     const test3 = require("../test/test3");
